f0cf8031872d33f437f736f633b36a15360bb4b5
murrayegger <murrayegger@de4c7b76-231b-0410-8e77-612b89767290>
2006-08-18 04:06:57 +0000

    Download 7z and use build.bat to update the xpi.

---
+++ trunk/build.bat
@@ -0,0 +1,14 @@
+set x=%cd%
+md build\chrome
+cd chrome
+7z a -tzip "%x%.jar" * -r -mx=0
+move "%x%.jar" ..\build\chrome
+cd ..
+copy install.* build
+copy chrome.manifest build
+copy components build
+cd build
+7z a -tzip "%x%.xpi" * -r -mx=9
+move "%x%.xpi" ..\
+cd ..
+rd build /s/q
