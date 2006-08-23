357421b190b32bdc24e5029381c39cd2fab57bef
murrayegger <murrayegger@de4c7b76-231b-0410-8e77-612b89767290>
2006-08-23 02:51:35 +0000

    Fixed build.bat to exclude thumbs.db and .svn folders.

--- build.bat
+++ build.bat
@@ -2,14 +2,14 @@ set x=%cd%
 md build\chrome
 md build\components
 cd chrome
-7z a -tzip "%x%.jar" * -r -mx=0
+7z a -tzip "%x%.jar" * -r -mx=0 -xr!.svn -x!Thumbs.db
 move "%x%.jar" ..\build\chrome
 cd ..
 copy install.* build
 copy chrome.manifest build
 copy components build\components
 cd build
-7z a -tzip "%x%.xpi" * -r -mx=9
+7z a -tzip "%x%.xpi" * -r -mx=9 
 move "%x%.xpi" ..\
 cd ..
 rd build /s/q
