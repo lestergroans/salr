10b542645f2e9bf7867296f06f067affffce68eb
pcavit <pcavit@de4c7b76-231b-0410-8e77-612b89767290>
2007-04-11 07:02:37 +0000

    uses xcopy /E to move over skin and defaults directories.  Now build.bat produces an xpi with an icon and all the proper default prefs every time.  It also ignores the useless update dir.

--- branches/2.0/build.bat
+++ branches/2.0/build.bat
@@ -3,7 +3,8 @@ set PATH=%PATH%;C:\Program Files\7-Zip
 set x=salastread
 md build\chrome
 md build\components
-md build\defaults\preferences
+md build\defaults
+md build\skin
 cd chrome
 7z a -tzip "%x%.jar" * -r -mx=0 -xr!.svn -x!Thumbs.db
 move "%x%.jar" ..\build\chrome
@@ -11,7 +12,8 @@ cd ..
 copy install.* build
 copy chrome.manifest-jar build\chrome.manifest
 copy components build\components
-copy defaults\preferences build\defaults\preferences
+xcopy /E defaults build\defaults
+xcopy /E skin build\skin
 cd build
 7z a -tzip "%x%.xpi" * -r -mx=9 
 move "%x%.xpi" ..\
