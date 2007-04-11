318f5670d84eda8aaee6db6f864d58a02eb48b05
pcavit <pcavit@de4c7b76-231b-0410-8e77-612b89767290>
2007-04-11 06:18:28 +0000

    build script for windows now includes default prefs

--- branches/2.0/build.bat
+++ branches/2.0/build.bat
@@ -3,6 +3,7 @@ set PATH=%PATH%;C:\Program Files\7-Zip
 set x=salastread
 md build\chrome
 md build\components
+md build\defaults\preferences
 cd chrome
 7z a -tzip "%x%.jar" * -r -mx=0 -xr!.svn -x!Thumbs.db
 move "%x%.jar" ..\build\chrome
@@ -10,6 +11,7 @@ cd ..
 copy install.* build
 copy chrome.manifest-jar build\chrome.manifest
 copy components build\components
+copy defaults\preferences build\defaults\preferences
 cd build
 7z a -tzip "%x%.xpi" * -r -mx=9 
 move "%x%.xpi" ..\
