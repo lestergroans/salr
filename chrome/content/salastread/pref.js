91eb8f41f9f09ba26fae4051ba8a981ae3f14ed3
pcavit <pcavit@de4c7b76-231b-0410-8e77-612b89767290>
2007-05-18 00:58:53 +0000

    New prefwindow and prefpane-based preferences window
    pref!

--- chrome/content/salastread/pref.js
+++ chrome/content/salastread/pref.js
@@ -35,39 +35,6 @@ ExtPrefs.prototype = {
 	}
 };
 
-//set up header text for each options dialog section
-function initPage(pageName) {
-	var header = document.getElementById("header");
-	header.setAttribute("title", pageName);
-}
-
-//change pages in the options dialog
-function switchPage(aButtonID) {
-	var button = document.getElementById(aButtonID);
-	document.getElementById("panelFrame").setAttribute("src", button.getAttribute("url"));
-}
-
-//called when "OK" button is clicked on the options dialog
-function saveChanges() {
-	prefobj.SetSettings();
-	if (prefobj.NeedMenuRebuild == true || prefobj.NeedFirefoxRestart == true) {
-		alert("Some of the changes you made will require you to restart Firefox to take effect.");
-	}
-}
-
-//tries to click a button if a command is passed from SA menu (WEIRD)
-function onLoad() {
-	var fp = window.arguments.length ? window.arguments[0] : false;
-	
-	if (typeof(fp) == "string" && fp.length > 0 && fp.substring(0, 3) == "cat" ) {
-		document.getElementById(fp).focus();
-		document.getElementById(fp).click();
-	} else {
-		document.getElementById("catGeneralButton").focus();
-		document.getElementById("catGeneralButton").click();
-	}
-}
-
 //initialize prefs object and grab settings
 try {
 	var prefobj = new ExtPrefs();
