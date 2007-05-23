d63ae531bf216eb4bbe8189bc66edbb54cd57ba0
dq5studios <dq5studios@de4c7b76-231b-0410-8e77-612b89767290>
2007-05-23 05:22:23 +0000

    Helps if I update the version number as well

--- branches/2.0/defaults/preferences/salastread.js
+++ branches/2.0/defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.070521');
+pref('extensions.salastread.currentVersion', '1.99.070522');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
